1f94bca0f22a929087382982e8862be9cd289d60
manjeetkmr18 <manjeet.kmr18@gmail.com>
2023-06-11 19:23:39 -0400

    bootstrap added

---
+++ src/MyComponent.tsx
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const MyComponent = () => {
+  return (
+    <div className="container">
+      <h1>My Next.js App with Bootstrap</h1>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
+};
+
+export default MyComponent;

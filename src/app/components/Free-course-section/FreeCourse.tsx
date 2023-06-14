d1f0e7fb9d856d9f1100936cb36a679219436b07
Jaskaran Singh <jaskaransingh1826@conestogac.on.ca>
2023-06-13 22:11:09 -0400

    homepage done

---
+++ src/app/components/Free-course-section/FreeCourse.tsx
@@ -0,0 +1,96 @@
+"use client";
+import React from "react";
+
+
+import courseImg01 from "../../assests/web-development.png";
+import courseImg02 from "../../assests/kids-learning.png";
+import courseImg03 from "../../assests/seo.png";
+import courseImg04 from "../../assests/ui-ux.png";
+import FreeCourseCard from "./FreeCourseCard";
+
+import "./free-course.css";
+
+const freeCourseData = [
+  {
+    id: "01",
+    title: "Basic Web Development Course",
+    imgUrl: courseImg01,
+    students: 5.3,
+    rating: 1.7,
+  },
+  {
+    id: "02",
+    title: "Coding for Junior Basic Course",
+    imgUrl: courseImg02,
+    students: 5.3,
+    rating: 1.7,
+  },
+
+  {
+    id: "03",
+    title: "Search Engine Optimization",
+    imgUrl: courseImg03,
+    students: 5.3,
+    rating: 1.7,
+  },
+
+  {
+    id: "04",
+    title: "Basic UI/UX Design - Figma",
+    imgUrl: courseImg04,
+    students: 5.3,
+    rating: 1.7,
+  },
+  {
+    id: "05",
+    title: "Basic Web Development Course",
+    imgUrl: courseImg01,
+    students: 5.3,
+    rating: 1.7,
+  },
+  {
+    id: "06",
+    title: "Coding for Junior Basic Course",
+    imgUrl: courseImg02,
+    students: 5.3,
+    rating: 1.7,
+  },
+
+  {
+    id: "07",
+    title: "Search Engine Optimization",
+    imgUrl: courseImg03,
+    students: 5.3,
+    rating: 1.7,
+  },
+
+  {
+    id: "08",
+    title: "Basic UI/UX Design - Figma",
+    imgUrl: courseImg04,
+    students: 5.3,
+    rating: 1.7,
+  },
+];
+
+const FreeCourse = () => {
+  return (
+    <section className="free_class">
+      
+        <div className="row">
+          <div className="col col-lg-12 col-md-12 text-center mb-5">
+            <h2 className="fw-bold">Our Free Classes</h2>
+          </div>
+
+          {freeCourseData.map((item) => (
+            <div className="col col-lg-3 col-md-12 mb-5" key={item.id}>
+              <FreeCourseCard item={item} />
+            </div>
+          ))}
+        </div>
+      
+    </section>
+  );
+};
+
+export default FreeCourse;

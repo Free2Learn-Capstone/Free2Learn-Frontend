41eabb094952fc65399ab7fb62cbd5b2ef5380b8
KiraLei <mirian-estrella18@outlook.es>
2023-07-04 15:23:54 -0400

    privacy progress

---
+++ src/app/components/Privacy-policy/page.tsx
@@ -0,0 +1,9 @@
+import PrivacyPolicy from "./PrivacyPolicy"
+
+
+export default function Page() {
+    return (
+        <div className="container">
+        <PrivacyPolicy></PrivacyPolicy>
+        </div>
+  )}

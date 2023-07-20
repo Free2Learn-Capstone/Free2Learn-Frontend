d69b8bf666dbf4a990fba9224f3095e7aa30c2c2
KiraLei <mirian-estrella18@outlook.es>
2023-07-20 17:54:54 -0400

    Fixing variable access for login

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET_KEY } = process.env; 
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY; 
 
 const verifyTokenAndGetRole = (token) => {
   try {
